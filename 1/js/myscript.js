d5901557eb5659f91577181a7ac92c6d5472813f
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-22 10:11:09 +0100

    add functionality even and odd on html

---
+++ 1/js/myscript.js
@@ -0,0 +1,40 @@
+
+/**
+Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
+ Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+*/
+
+
+
+const { createApp } = Vue;
+
+createApp({
+    data()  {
+        return {
+            pari: [],
+            dispari: [],
+        }
+    },
+    methods: {
+        getNumber(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+            .then( (response) => {
+                if (response.data.response % 2 === 0){
+                    this.pari.push(response.data.response)
+                    
+                } else {
+                    this.dispari.push(response.data.response)
+                   
+                }
+            })
+        }
+    },
+    created(){
+        
+    }
+}).mount('#app')
+
+
+
+// axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
